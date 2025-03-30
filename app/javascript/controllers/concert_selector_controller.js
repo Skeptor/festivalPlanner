0dc7246c97b290571dc7e69f0a0e831ae7f067fc
Skeptor <pablo.navgui@gmail.com>
2025-03-30 11:23:15 +0200

    Simplify toggle selection

--- app/javascript/controllers/concert_selector_controller.js
+++ app/javascript/controllers/concert_selector_controller.js
@@ -3,17 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
     static targets = ["input"];
 
-    toggleSelection(concertId) {
-        const concertDiv = this.element;
-        const concertIdValue = concertDiv.dataset.concertId;
-        const input = this.inputTargets.find(input => input.value == concertIdValue);
-
-        if (concertDiv.classList.contains("selected")) {
-            concertDiv.classList.remove("selected");
-            input.disabled = true;
-        } else {
-            concertDiv.classList.add("selected");
-            input.disabled = false;
-        }
+    toggleSelection() {
+        this.element.classList.toggle("selected");
     }
 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_09_184543) do
+ActiveRecord::Schema[8.0].define(version: 2024_10_09_184543) do
   create_table "agenda_concerts", force: :cascade do |t|
     t.integer "agenda_id"
     t.integer "concert_id"
